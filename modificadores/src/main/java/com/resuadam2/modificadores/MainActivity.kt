4571a060d6d763d04c1c6fab390f880c4817dff1
resuadam2 <resuadam2@gmail.com>
2024-09-18 14:26:06 +0200

    starting

---
+++ modificadores/src/main/java/com/resuadam2/modificadores/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.resuadam2.modificadores
+
+/**
+ * Created by resuadam2 on 31/01/2022.
+ * Tutorial de Compose en Android
+ * Basado en la lista de tutoriales de Mouredev y en la documentación oficial de Android sobre Compose
+ */
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.resuadam2.modificadores.ui.theme.TutorialComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TutorialComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TutorialComposeTheme {
+        Greeting("Android")
+    }
+}
